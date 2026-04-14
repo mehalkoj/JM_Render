3a1f218b5d6e81fbce557f8568566faa361b8df0
mehalkoj <mehalkoj@gmail.com>
2026-04-14 10:12:37 -0500

    Obj File Vertices Extraction

--- mdl_importer.cpp
+++ mdl_importer.cpp
@@ -16,13 +16,33 @@ void importer() {
 
 	std::cout << "Loading In Model!" << std::endl;
 	while (std::getline(file, line)) {
-		char myChar = file.get();
-		std::getline(file >> std::ws, line);
-		if (myChar == 118) {
-			//std::cout << line << std::endl;
+		// vertices
+		if (line.substr(0, 2) == "v ") {
+			std::istringstream v(line.substr(2));
+			float x, y, z;
+			v >> x >> y >> z;
+			Vertex vert;
+			vert.pos = { x, y, z };
+			verts.push_back(vert);
+			//std::cout << x << " " << y << " " << z << std::endl;
 		}
-		else if (myChar == 102) {
-			//std::cout << line << std::endl;
+		
+		// texture coordinate
+		else if (line.substr(0, 2) == "vt") {
+			std::istringstream v(line.substr(3));
+
+		}
+
+		// vertex normals
+		else if (line.substr(0, 2) == "vn") {
+			std::istringstream v(line.substr(3));
+
+		}
+
+		// faces
+		else if (line.substr(0, 2) == "f ") {
+			int a, b, c;
+			int A, B, C;
 		}
 	}
 	std::cout << "Done!" << std::endl;
--- mdl_importer.h
+++ mdl_importer.h
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <sstream>
 #include "JM_Math.h"
 
 
